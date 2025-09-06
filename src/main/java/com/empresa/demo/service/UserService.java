81aabc20dd975381066d23207d1fc48287117bb6
afmejia <afmejia23@utp.edu.co>
2025-09-06 16:14:41 -0500

    Annotate UserService with @Service to fix missing bean error

--- src/main/java/com/empresa/demo/service/UserService.java
+++ src/main/java/com/empresa/demo/service/UserService.java
@@ -1,7 +1,10 @@
 package com.empresa.demo.service;
 
+import org.springframework.stereotype.Service;
+
 import com.empresa.demo.model.User;
 
+@Service
 public class UserService {
     public User findById(Long id) {
         // TODO: simular recuperación (p.ej. base de datos)
